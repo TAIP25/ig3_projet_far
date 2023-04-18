d0b2546400bb3c0d6d8aa86b6cfb78c78a162b16
BOUDIER Leon <leon.boudier@IGTP5-PC10.isim.intra>
2023-04-18 15:13:32 +0200

    Ajout de l'arborescence

---
+++ sprint1/seance2-3-4/client.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define MAXCHARS 255
+
+int main(int argc, char *argv[]) {
+
+    //VERIFICATION DU NOMBRE D'ARGUMENTS
+    //argv[0] = client
+    //argv[1] = adresse IP du serveur
+    //argv[2] = port du serveur
+    //argv[3] = le numéro du client (1 ou 2)
+    if (argc != 4) {
+        printf("Erreur nombre d'argument\n");
+        printf("Usage: %s <adresse IP> <port> <numéro du client>\n", argv[0]);
+        exit(0);
+    }
+
+    //CREATION DE LA SOCKET DU CLIENT
+
+    //int socket(int domaine, int type, int protocole)
+    //Renvoie le descripteur de socket si la création est réussi et -1 si elle échoue
+    //PF_INET = Protocole IP
+    //SOCK_STREAM = Protocole TCP
+    //0 = Protocole par défaut
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //gestion d'erreur
+    if (client_socket == -1) {
+        perror("Erreur de création de socket client");
+        exit(0);
+    }
+    printf("Socket Créé\n");
+
+
+    //DEFINITION DE L'ADRESSE IP ET DU PORT D'ECOUTE DU SERVEUR
+
+    //adresse du serveur
+    struct sockaddr_in server_address;
+
+    //Stock la famille d'adresse dans la structure aS
+    //AF_INET = Protocole IP
+    server_address.sin_family = AF_INET;
+
+    //Converti l'adresse IP en format réseau
+    //inet_pton(int af, const char *src, void *dst)
+    //Renvoie 1 si la conversion est réussi et 0 si elle échoue
+    //INADDR_ANY = adresse IP de la machine
+    //argv[1] = adresse IP du serveur
+    //server_adress.sin_addr = adresse du serveur
+    inet_pton(AF_INET,argv[1],&(server_address.sin_addr));
+
+    //Converti le port en format réseau
+    //unsigned short htons(unsigned short hostshort)
+    //atoi() is a function in the C standard library that converts a string of characters representing an integer value to an actual integer value.
+    //Renvoie le port en format réseau en cas de succès et 0 en cas d'échec
+    //argv[2] = port du serveur
+    server_address.sin_port = htons(atoi(argv[2]));
+
+    //Stocke la taille de la structure aS dans lgA pour la fonction connect()
+    socklen_t lgA = sizeof(struct sockaddr_in);
+
+    //Connecte la socket au serveur
+    //connect(int dS, struct sockaddr *aS, socklen_t lgA)
+    //Renvoie 0 si la connexion est réussi et -1 si elle échoue
+    //dS = descripteur de socket
+    //aS = adresse du serveur
+    //lgA = taille de la structure aS
+    int connection = connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+    if (connection == -1) {
+        perror("Erreur lors de la connexion du client au serveur");
+        exit(0);
+    }
+
+    //CREATION DE LA CONVERSATION
+    
+    if (atoi(argv[3]) == 1) {
+        printf("Vous êtes le client 1, vous commencez la conversation\n");
+    }
+    else{
+        printf("Vous êtes le client 2, vous attendez la réponse du client 1\n");
+    }
+    
+    // Boucle infinie pour la conversation
+    while (1) {
+
+        char message[MAXCHARS] = {0};
+
+        // Si c'est le client 1, il commence la conversation
+        if (atoi(argv[3]) == 1) {
+            printf("Entrez votre message : ");
+            
+            fgets(message, MAXCHARS, stdin);
+            if(message[strlen(message) - 1] == '\n'){
+                message[strlen(message) - 1] = '\0';
+            }
+
+            if(strlen(message) > MAXCHARS){
+                printf("Erreur message trop grand\n");
+            }
+            else{
+
+                //Envoie le message au serveur
+                //int send(int dS, const void *m, size_t lg, int flags)
+                //Renvoie le nombre d'octet envoyé si la connexion est réussi et -1 si elle échoue
+                //dS = descripteur de socket
+                //m = message
+                //strlen(m) + 1 = taille du message
+                //0 = protocole par défaut
+                send(client_socket, message, strlen(message) + 1 , 0);
+                
+                printf("Message envoyé\n");
+
+                //Le client attend la réponse du serveur
+                printf("En attente de la réponse du client 2\n");
+
+                int recvR = recv(client_socket, message, sizeof(message), 0) - 1;
+
+                if (recvR == -1 || strcmp(message, "fin") == 0) {
+                    perror("Erreur lors de la réception du message ou fin de la conversation");
+                    exit(0);
+                }
+
+                printf("Réponse du client 2 : %s\n", message);
+            }
+
+        }
+        else{
+
+            //Le client attend la réponse du serveur
+            int recvR = recv(client_socket, message, sizeof(message), 0) - 1;
+
+            if (recvR == -1 || strcmp(message, "fin") == 0) {
+                perror("Erreur lors de la réception du message ou fin de la conversation");
+                exit(0);
+            }
+
+            printf("Réponse du client 1 : %s\n", message);
+
+            printf("Entrez votre message : ");
+
+            fgets(message, MAXCHARS, stdin);
+            if(message[strlen(message) - 1] == '\n'){
+                message[strlen(message) - 1] = '\0';
+            }
+
+            if(strlen(message) > MAXCHARS){
+                printf("Erreur message trop grand\n");
+            }
+            else{
+
+                //Envoie le message au serveur
+                //int send(int dS, const void *m, size_t lg, int flags)
+                //Renvoie le nombre d'octet envoyé si la connexion est réussi et -1 si elle échoue
+                //dS = descripteur de socket
+                //m = message
+                //strlen(m) + 1 = taille du message
+                //0 = protocole par défaut
+                send(client_socket, message, strlen(message) + 1 , 0);
+                
+                printf("Message envoyé\n");
+
+                //Le client attend la réponse du serveur
+                printf("En attente de la réponse du client 1\n");
+            }
+        }
+    }
+
+    // Fermeture de la socket du client
+    close(client_socket);
+
+    return 0;
+
+}   
---
+++ sprint1/seance2-3-4/serveur.c
@@ -0,0 +1,170 @@
+    #include <stdio.h>
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <stdlib.h>
+    #include <string.h>
+
+int main(int argc, char *argv[]) {
+
+    //Vérification du nombre d'argument
+    //argv[0] = port du serveur
+    if(argc != 2){
+        printf("Erreur nombre d'argument\n");
+        exit(0);
+    }
+
+    printf("Début programme\n");
+    
+    //Création de la structure aS
+    //aS = adresse du serveur
+    struct sockaddr_in aS;
+
+    //Stocke la famille d'adresse dans la structure aS
+    //AF_INET = Protocole IP
+    aS.sin_family = AF_INET;
+
+    //Converti l'adresse IP en format réseau
+    //INADDR_ANY = adresse IP de la machine
+    aS.sin_addr.s_addr = INADDR_ANY ;
+
+    //Converti le port en format réseau
+    //unsigned short htons(unsigned short hostshort)
+    //Renvoie le port en format réseau en cas de succès et 0 en cas d'échec
+    //argv[1] = port du serveur
+    aS.sin_port = htons(atoi(argv[1])) ;
+
+    //Création du socket pour les clients
+    //int socket(int domaine, int type, int protocole)
+    //Renvoie le descripteur de socket si la création est réussi et -1 si elle échoue
+    //dS = descripteur de socket
+    //PF_INET = Protocole IP
+    //SOCK_STREAM = Protocole TCP
+    //0 = Protocole par défaut
+    int dS = socket(PF_INET, SOCK_STREAM, 0);
+    
+    printf("Le socket est créé\n"); 
+
+    //Liaison de la socket au serveur
+    //int bind(int dS1, struct sockaddr *aS, socklen_t lgA)
+    //Renvoie 0 si la liaison est réussi et -1 si elle échoue
+    //dS1 = descripteur de socket
+    //&aS = adresse du serveur
+    //sizeof(aS) = taille de la structure aS
+    bind(dS, (struct sockaddr*)&aS, sizeof(aS));
+
+    printf("Le socket est nommé\n");
+
+    //Mise en écoute de la socket
+    //int listen(int dS, int nbC)
+    //Renvoie 0 si la mise en écoute est réussi et -1 si elle échoue
+    //dS = descripteur de socket
+    //nbC = nombre de connexion en attente
+    listen(dS, 2);
+
+    printf("Mode écoute\n");
+
+    while(1){
+        //Création de la structure aC
+        //aC = adresse du client
+        struct sockaddr_in aC1;
+        struct sockaddr_in aC2;
+
+        //Stocke la taille de la structure aC dans lg
+        socklen_t lg1 = sizeof(struct sockaddr_in);
+        socklen_t lg2 = sizeof(struct sockaddr_in);
+
+        //Accepte la connexion du client
+        //int accept(int dS, struct sockaddr *aC, socklen_t *lg)
+        //Renvoie le descripteur de socket du client
+        //dS = descripteur de socket
+        //&aC = adresse du client
+        //&lg = l'adresse de la taille de la structure aC
+        int dSC1 = accept(dS, (struct sockaddr*) &aC1,&lg1);
+        printf("Le client 1 est connecté\n");
+
+        int dSC2 = accept(dS, (struct sockaddr*) &aC2,&lg2);
+        printf("Le client 2 est connecté\n");
+
+        char msg[255];
+
+        int fin = 0;
+
+        while (!fin) {
+            //Reçoit un message du client 1
+            //int recv(int dSC, void *msg, int lg, int flags)
+            //Renvoie le nombre d'octet reçu
+            //dSC = descripteur de socket du client
+            //msg = message reçu
+            //sizeof(msg) = taille du message (ici 32)
+            //0 = flags
+            //int recvR1 = recv(dSC1, msg1, sizeof(msg1), 0) - 1;
+            //int recvR2 = recv(dSC2, msg2, sizeof(msg2), 0) - 1;
+            int recvC1 = recv(dSC1, msg, sizeof(msg), 0) - 1;
+
+            //Vérifie si la connexion est interrompu ou si une erreur est survenue
+            if(recvC1 == 0 || recvC1 == -1){
+                perror("Erreur interruption de la connexion ou erreur du client 1");
+                break;
+            }
+            else if (strcmp(msg, "fin") == 0) {
+                fin = 1;
+                printf("Le client 1 ferme la connexion\n");
+            }
+
+            printf("Message reçu : %s\n", msg);
+
+            //Envoie un message au client 2
+            //int send(int dSC, void *msg, int lg, int flags)
+            //Renvoie le nombre d'octet envoyé
+            //dSC = descripteur de socket du client
+            //&recvR = message envoyé
+            //sizeof(int) = taille du message (ici 4)
+            //0 = flags
+            //send(dSC1, &recvR, sizeof(int), 0);
+            send(dSC2, msg, sizeof(msg), 0);
+
+            printf("Message envoyé\n");
+
+            //Reçoit un message du client 2
+            int recvC2 = recv(dSC2, msg, sizeof(msg), 0) - 1;
+
+            //Vérifie si la connexion est interrompu ou si une erreur est survenue
+            if(recvC2 == 0 || recvC2 == -1){
+                perror("Erreur interruption de la connexion ou erreur du client 2");
+                break;
+            }
+            else if (strcmp(msg, "fin") == 0) {
+                fin = 1;
+                printf("Le client 2 ferme la connexion\n");
+            }
+
+            printf("Message reçu : %s\n", msg);
+
+            //Envoie un message au client 1
+            send(dSC1, msg, sizeof(msg), 0);
+
+            printf("Message envoyé\n");
+        }
+        printf("Fin de la conversation\n");
+
+        //Ferme la connexion du client
+        //int shutdown(int dSC, int mode)
+        //Renvoie 0 si la fermeture est réussi et -1 si elle échoue
+        //dSC = descripteur de socket du client
+        //2 = fermeture de la connexion
+        shutdown(dSC1, 2); 
+        shutdown(dSC2, 2);
+
+        printf("Nouvelle conversation possible, en attente de client\n");
+
+    }
+
+    //Ferme la connexion du serveur
+    //int shutdown(int dS1, int mode)
+    //Renvoie 0 si la fermeture est réussi et -1 si elle échoue
+    //dS1 = descripteur de socket
+    //2 = fermeture de la connexion
+    shutdown(dS, 2);
+
+    printf("Fin du programme\n");
+}
